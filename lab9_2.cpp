78d19b16eb0ac4c8f306a8ba05288452a82d842d
pondcmu <poncmu@gmail.com>
2020-01-28 16:16:21 +0700

    Pond

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,25 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    ifstream cheerbook;
+    cheerbook.open("C:\\Users\\ASUS G531GT\\Desktop\\lab9-2562-2-poncmu\\cheerbook.txt");
+    string text;
+    ofstream cheerbook2;
+    cheerbook2.open("C:\\Users\\ASUS G531GT\\Desktop\\lab9-2562-2-poncmu\\cheerbook_copy.txt");
+    cheerbook2<<"-------------------- SOTUS ---------------------"<<endl;
+    while(getline(cheerbook,text))
+    {
+        cheerbook2<<text<<endl;
+    }
+    cheerbook2<<"-------------------- SOTUS ---------------------"<<endl;
+    
+    
+
+    return 0;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    fstream score;
+    score.open("score.txt");
+    string textline;
+    double sum1=0;
+    double sum2=0;
+    int c=0;
+   
+    while(getline(score,textline))
+    {
+        sum1=sum1+atof(textline.c_str());
+        sum2=sum2+pow(atof(textline.c_str()),2);
+        c++;
+    }
+    cout<<"N = "<<c<<endl;
+    cout<<"Mean = "<<sum1/c<<endl;
+
+    cout<<"Standard deviation = "<<pow(sum2/c-pow(sum1/c,2),0.5);
+
+
+    return 0;
+}
